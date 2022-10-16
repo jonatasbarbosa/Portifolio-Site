3bd20c55f9bfe3f4b8d416e8316df693d28a9981
Jonatas Barbosa <jonh_stars8@hotmail.com>
2022-10-16 20:35:31 -0300

    funcionalidades

--- js/main.js
+++ js/main.js
@@ -3,3 +3,30 @@ hamburguer.addEventListener("click", function () {
     document.querySelector(".container").classList.toggle("show-menu");
 
 });
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function () {
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    atualizarPreco()
+})
+function atualizarPreco() {
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked
+    const prazo = document.querySelector("#prazo").value
+    const lysim = document.querySelector("#layout-sim").checked
+    const lynao = document.querySelector("#layout-nao").checked
+
+    let preco = qtde * 100;
+    if (temJS) {
+        preco = preco + (preco * 10 / 100)
+    }
+    if (lysim) {
+        preco += 500
+    }
+    let urge = 1 - prazo * 0.1;
+    preco = preco + (preco * urge)
+    document.querySelector("#preco").innerHTML = `R$ ${preco}`
+}
